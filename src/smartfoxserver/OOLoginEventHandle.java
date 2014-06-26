9edfe04439ee6a59c33f9ba9cfa089910702fba7
BruceZCQ <brucezcq@gmail.com>
2014-06-26 16:06:51 +0800

    get login params

--- src/smartfoxserver/OOLoginEventHandle.java
+++ src/smartfoxserver/OOLoginEventHandle.java
@@ -6,6 +6,8 @@ package smartfoxserver;
 
 
 import com.smartfoxserver.v2.core.ISFSEvent;
+import com.smartfoxserver.v2.core.SFSEventParam;
+import com.smartfoxserver.v2.entities.data.SFSObject;
 import com.smartfoxserver.v2.exceptions.SFSException;
 import com.smartfoxserver.v2.extensions.BaseServerEventHandler;
 
@@ -21,6 +23,8 @@ public class OOLoginEventHandle extends BaseServerEventHandler {
 	@Override
 	public void handleServerEvent(ISFSEvent event) throws SFSException {
 		System.out.println("===custom login====");
+		SFSObject o = (SFSObject)event.getParameter(SFSEventParam.LOGIN_IN_DATA);
+		System.out.println("event ==="+event+o.getUtfString("zcq"));
 	}
 
 }

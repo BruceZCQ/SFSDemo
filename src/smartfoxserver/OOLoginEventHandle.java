9df0adc8c5af868a1d8ef82a207616701ff464b9
BruceZCQ <brucezcq@gmail.com>
2014-06-26 18:10:01 +0800

    ss

--- src/smartfoxserver/OOLoginEventHandle.java
+++ src/smartfoxserver/OOLoginEventHandle.java
@@ -7,6 +7,8 @@ package smartfoxserver;
 
 import com.smartfoxserver.v2.core.ISFSEvent;
 import com.smartfoxserver.v2.core.SFSEventParam;
+import com.smartfoxserver.v2.entities.SFSUser;
+import com.smartfoxserver.v2.entities.User;
 import com.smartfoxserver.v2.entities.data.SFSObject;
 import com.smartfoxserver.v2.exceptions.SFSException;
 import com.smartfoxserver.v2.extensions.BaseServerEventHandler;
@@ -24,7 +26,16 @@ public class OOLoginEventHandle extends BaseServerEventHandler {
 	public void handleServerEvent(ISFSEvent event) throws SFSException {
 		System.out.println("===custom login====");
 		SFSObject o = (SFSObject)event.getParameter(SFSEventParam.LOGIN_IN_DATA);
-		System.out.println("event ==="+event+o.getUtfString("zcq"));
+		
+		String logoutUser = o.getUtfString("zcq");
+		
+		User user = this.getApi().getUserByName(logoutUser);
+		if (!user.equals(null)){
+
+			this.getApi().logout(user);
+		}
+		
+		System.out.println("event ==="+event+logoutUser);
 	}
 
 }
